79d6adb0ad4a689f5361c4512ec3cbc1472f0c33
DavidWu <785232598@qq.com>
2025-07-15 14:48:59 +0800

    Update UIFont+.swift

--- EFFoundation/Core/Extension/UIFont+.swift
+++ EFFoundation/Core/Extension/UIFont+.swift
@@ -13,35 +13,35 @@ public extension UIFont {
 
     // MARK: - PingFangSC
     static func pingFangSCRegular(ofSize: CGFloat) -> UIFont {
-        return UIFont.init(name: "PingFangSC-Regular", size: ofSize) ?? UIFont.systemFont(ofSize: ofSize)
+        return UIFont.systemFont(ofSize: ofSize, weight: .regular)
     }
 
     static func pingFangSCUltralight(ofSize: CGFloat) -> UIFont {
-        return UIFont.init(name: "PingFangSC-Ultralight", size: ofSize) ?? UIFont.pingFangSCRegular(ofSize: ofSize)
+        return UIFont.systemFont(ofSize: ofSize, weight: .ultraLight)
     }
 
     static func pingFangSCLight(ofSize: CGFloat) -> UIFont {
-        return UIFont.init(name: "PingFangSC-Light", size: ofSize) ?? UIFont.pingFangSCUltralight(ofSize: ofSize)
+        return UIFont.systemFont(ofSize: ofSize, weight: .light)
     }
 
     static func pingFangSCThin(ofSize: CGFloat) -> UIFont {
-        return UIFont.init(name: "PingFangSC-Thin", size: ofSize) ?? UIFont.pingFangSCLight(ofSize: ofSize)
+        return UIFont.systemFont(ofSize: ofSize, weight: .thin)
     }
 
     static func pingFangSCMedium(ofSize: CGFloat) -> UIFont {
-        return UIFont.init(name: "PingFangSC-Medium", size: ofSize) ?? UIFont.pingFangSCThin(ofSize: ofSize)
+        return UIFont.systemFont(ofSize: ofSize, weight: .medium)
     }
 
     static func pingFangSCSemibold(ofSize: CGFloat) -> UIFont {
-        return UIFont.init(name: "PingFangSC-Semibold", size: ofSize) ?? UIFont.pingFangSCMedium(ofSize: ofSize)
+        return UIFont.systemFont(ofSize: ofSize, weight: .semibold)
     }
 
     static func pingFangSCBold(ofSize: CGFloat) -> UIFont {
-        return UIFont.init(name: "PingFangSC-Bold", size: ofSize) ?? UIFont.pingFangSCSemibold(ofSize: ofSize)
+        return UIFont.systemFont(ofSize: ofSize, weight: .bold)
     }
 
     static func pingFangSCBoldItalic(ofSize: CGFloat) -> UIFont {
-        return UIFont.init(name: "PingFangSC-BoldItalic", size: ofSize) ?? UIFont.pingFangSCBold(ofSize: ofSize)
+        return UIFont.systemFont(ofSize: ofSize, weight: .bold)
     }
 }
 #endif
